e74f5af836f6460156db5d33c3de2e179381aaf2
Jess Manapul <Jess@Jess.home>
2019-02-22 19:51:30 -1000

    did #1

--- js/app.js
+++ js/app.js
@@ -3,6 +3,21 @@
 
 /*1) to the navmenu items (Home, Location, Blog) that will show and hide the contents for that specific navmenu item.*/
 
+var navItems = document.getElementsByClassName('navi');
+
+for (var i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener('click', showMenu);
+}
+
+function showMenu() {
+    var item = this.querySelector('.inner');
+    
+    if (item.style.display === 'block') {
+        item.style.display = 'none';
+    } else {
+        item.style.display = 'block';
+    }
+}
 
 /*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
